43007d5697ec70c049c613e560d4bedf59fb4401
JustinFrost47 <rinojonathanarul@gmail.com>
2024-08-27 10:16:03 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button"
+
+
+
+
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_TEMP)
+  return (
+    <>
+    <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
