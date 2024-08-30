02fc218eb179dce0453479bb88eae1f04b32a625
JustinFrost47 <rinojonathanarul@gmail.com>
2024-08-31 02:13:42 +0530

    getAll nfts incomplete implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import AllNfts from './pages/nfts/AllNfts'
+import MintNfts from './pages/nfts/MintNfts'
 
 import './App.css'
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className=' w-full'>
       <AllNfts/>
+      <MintNfts/>
     </div>
   )
 }
--- src/pages/nfts/AllNfts.tsx
+++ src/pages/nfts/AllNfts.tsx
@@ -1,13 +1,47 @@
 import { Button } from "@/components/ui/button"
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
+import { mplTokenMetadata, fetchAllDigitalAssetByOwner } from '@metaplex-foundation/mpl-token-metadata'
+import { PublicKey, Connection } from "@solana/web3.js"
+import { TOKEN_PROGRAM_ID, AccountLayout } from '@solana/spl-token';
+
 // import React from 'react'
+const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+  'TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb' // Update this with the correct program ID for program 22
+);
+
 
 export default function AllNfts() {
+
+  const getNfts = async () => {
+    // Use the RPC endpoint of your choice.
+  const umi = createUmi(import.meta.env.VITE_RPC_URL).use(mplTokenMetadata())
+
+  const connection = new Connection(import.meta.env.VITE_RPC_URL)
+    // Convert the public key string to a PublicKey object
+    const publicKey = new PublicKey(import.meta.env.VITE_PUBLIC_KEY);
+
+    // Fetch token accounts by the owner
+    const tokenAccounts = await connection.getTokenAccountsByOwner(publicKey, {
+      programId: TOKEN_PROGRAM_ID,
+    });
+
+
+
+  const assets = await fetchAllDigitalAssetByOwner(umi, import.meta.env.VITE_PUBLIC_KEY)
+  console.log("hi")
+  console.log(tokenAccounts)
+
+
+  }
+
+  
+
   return (
     <div className="flex flex-row justify-center items-center min-h-96">
             <div className="nft-images ">
           
             </div>
-            <Button>Click me</Button>
+            <Button onClick={getNfts}>Click me</Button>
     </div>
   )
 }
