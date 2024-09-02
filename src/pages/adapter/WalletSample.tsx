3950cd75fb669efbb03c5f269eb0e3a2a5b6a35d
JustinFrost47 <rinojonathanarul@gmail.com>
2024-09-02 21:04:55 +0530

    implemented wallet adapter into codebase

---
+++ src/pages/adapter/WalletSample.tsx
@@ -0,0 +1,41 @@
+// MyComponent.js
+
+
+import { useWallet, useConnection } from '@solana/wallet-adapter-react';
+
+
+const WalletSample = () => {
+    const { wallet, connect, disconnect, connected, publicKey} = useWallet();
+    const { connection } = useConnection();
+
+    const handleConnect = async () => {
+        try {
+            await connect();
+        } catch (error) {
+            console.error('Failed to connect wallet:', error);
+        }
+    };
+
+    const handleDisconnect = async () => {
+        try {
+            await disconnect();
+        } catch (error) {
+            console.error('Failed to disconnect wallet:', error);
+        }
+    };
+
+    return (
+        <div>
+            {connected ? (
+                <div>
+                    <p>Connected with {wallet?.adapter.name} {publicKey?.toBase58()}</p>
+                    <div onClick={handleDisconnect}>Disconnect</div>
+                </div>
+            ) : (
+                <div onClick={handleConnect}>Connect Wallet</div>
+            )}
+        </div>
+    );
+};
+
+export default WalletSample;
